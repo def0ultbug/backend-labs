ed47d403310276788ea8f522884bdc5eb15f1c0b
def0ultbug <youssefmahfoudh185@gmail.com>
2026-02-27 11:25:00 +0100

    Add FastAPI traffic testing experiment

---
+++ fastapi/04_fastapi-test-traffic/fastapi_test_traffic/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+@app.get('/home')
+async def hello():
+    return {'message':' Hello World'}
+
+
+def main(): 
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
+if __name__ == "__main__":
+    main()
